2b5652d7f494e827ff6721eb19ede775ea3a6e05
Adebisola <adeniyiadebisola@gmail.com>
2022-09-08 13:49:01 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - this is the entry point and print the string in the printf function
+* Description: using the main function
+* this program will print "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
