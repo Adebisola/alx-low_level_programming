b679c25a28f9c00fd44f487746a9e2a8613cd899
Adebisola <adeniyiadebisola@gmail.com>
2022-09-09 13:16:20 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Description:Prints the alphabet in lowercase.
+* Return: EXIT_SUCCESS
+*/
+
+int main(void)
+{
+	/* local variable declaration */
+	int c = 0;
+/* while loop execution */
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+	putchar(c);
+putchar('\n');
+
+return (EXIT_SUCCESS);
+}
