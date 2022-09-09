5176a80902b123c9bbcea9b756d880f6b0d99e52
Adebisola <adeniyiadebisola@gmail.com>
2022-09-09 12:50:50 -0700

    Task 2 test

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,25 +3,19 @@
 #include <time.h>
 /**
 * main - Entry point
-* Description:program that prints numbers from 0 to 99.
-* Return: 0
+* Description:Prints the alphabet in lowercase.
+* Return: EXIT_SUCCESS
 */
 
 int main(void)
 {
 	/* local variable declaration */
-	int c = 0;
+	char c = 0;
 /* while loop execution */
-	while (c <= 99)
-	{
-		putchar(c / 10 + '0');
-		putchar(c % 10 + '0');
-		if (c != 99)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		c++;
-	}
-return (0);
+for (c = 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+putchar('\n');
+}
+return (EXIT_SUCCESS);
 }
