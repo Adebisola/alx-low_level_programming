c7207ed188bf56324f0b82c673053b5046b2b72c
Adebisola <adeniyiadebisola@gmail.com>
2022-09-09 13:34:45 -0700

    This is task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,16 @@
  */
 int main(void)
 {
-char alphabet;
-for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+/*local variable declaration*/
+int i;
+/*start of boolean operation*/
+for (i = 'a' ; i <= 'z' ; i++)
 {
-if (alphabet != 'e' && alphabet != 'q')
-{
-	putchar(alphabet);
-}
-}
-putchar('\n');
-return (EXIT_SUCCESS);
+if (i != 'e' && i != 'q')
+	putchar(i);
 
+putchar('\n');
+
+}
+return (0);
+}
