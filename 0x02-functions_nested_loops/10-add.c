47d932f064e8d3b0b5cef5761eef88ba56f25f7d
Adebisola <adeniyiadebisola@gmail.com>
2022-09-13 14:47:20 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - Entry point * Description: add function is used in a program
+* that adds two itegers and returns the result.
+* @a: the first integer
+* @b: the second integer
+* Return: a + b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
