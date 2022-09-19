aeda57a5bb527d699683564016698d518ec8b7f1
Adebisola <adeniyiadebisola@gmail.com>
2022-09-19 07:39:02 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* reset_to_98 - entry point
+* Description: reset_to_98 function takes
+* a pointer to an int as
+* parameter and updates the value it points to.
+* @n: pointer variable passed as argument.
+*
+* Return: Always 0.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
