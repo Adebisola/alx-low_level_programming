72649c2f085bf65f452944743b326a37463cf566
Adebisola <adeniyiadebisola@gmail.com>
2022-10-02 09:43:56 -0700

    task 0

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,20 @@
 #include "main.h"
-/**
-* _puts - function to use to print.
-* *s - string character.
-* Return: 0;
-*/ 
 
-void _puts(char *s)
+/**
+* _puts - Entry.
+* Description: the _puts function prints a string,
+* followed by a new line, to stdout.
+* @str: strin to print.
+* Return: Always 0.
+*/
+
+void _puts(char *str)
 {
 	int i;
 
-	for (i = 0; s[i] != 0; i++)
+	for (i = 0; str[i] != 0; i++)
 	{
-		_putchar(s[i]);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
