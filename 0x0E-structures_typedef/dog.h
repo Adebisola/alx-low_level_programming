ebbc88c7727d6d04b14f87666727935c30d4fdaf
Adebisola <adeniyiadebisola@gmail.com>
2022-10-10 08:43:54 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - structure for a dog.
+* @name: dog's name.
+* @age: dog's age.
+* @owner: dog's owner.
+*/
+
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+};
+
+
+#endif/* DOG_H */
