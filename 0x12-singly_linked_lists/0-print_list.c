3a0b0b238be2ea1b22967e7f91d80e892278104f
Adebisola <adeniyiadebisola@gmail.com>
2022-10-20 08:09:51 -0700

    Task 0 corrected

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,18 +11,13 @@ size_t print_list(const list_t *h)
 {
 	int i = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-			h = h->next;
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-		}
+		h = h->next;
 		i++;
 	}
 	return (i);
